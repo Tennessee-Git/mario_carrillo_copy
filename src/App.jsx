928cd0af669a090ce4b0fc6421907e9a895cc3b4
Paweł Awramiuk <mail@gmail.com>
2023-03-02 10:56:15 +0100

    Minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,72 @@ import { Block } from "./components/Block";
 import { Menu } from "./components/Menu";
 import "./styles/App.css";
 
+const blocks = [
+  {
+    size: "small",
+    title: "S1",
+    image: "https://picsum.photos/200?random=1",
+  },
+  {
+    size: "small",
+    title: "S1",
+    image: "https://picsum.photos/200?random=1",
+  },
+  {
+    size: "small",
+    title: "S2",
+    image: "https://picsum.photos/200?random=2",
+  },
+  {
+    size: "big",
+    title: "B1",
+    image: "https://picsum.photos/400?random=3",
+  },
+  {
+    size: "small",
+    title: "S3",
+    image: "https://picsum.photos/170?random=4",
+  },
+  {
+    size: "big",
+    title: "B2",
+    image: "https://picsum.photos/400?random=5",
+  },
+  {
+    size: "small",
+    title: "S4",
+    image: "https://picsum.photos/170?random=6",
+  },
+  {
+    size: "small",
+    title: "S5",
+    image: "https://picsum.photos/170?random=7",
+  },
+  {
+    size: "small",
+    title: "S6",
+    image: "https://picsum.photos/170?random=8",
+  },
+  {
+    size: "small",
+    title: "S7",
+    image: "https://picsum.photos/170?random=9",
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <div className="BlocksContainer">
         <div className="Blocks">
-          <Block
-            size="small"
-            title="S1"
-            image="https://picsum.photos/200?random=1"
-          ></Block>
-          <Block
-            size="small"
-            title="S2"
-            image="https://picsum.photos/200?random=2"
-          ></Block>
-          <Block
-            size="big"
-            title="B1"
-            image="https://picsum.photos/400?random=3"
-          ></Block>
-          <Block
-            size="small"
-            title="S3"
-            image="https://picsum.photos/170?random=4"
-          ></Block>
-          <Block
-            size="big"
-            title="B2"
-            image="https://picsum.photos/400?random=5"
-          ></Block>
-          <Block
-            size="small"
-            title="S4"
-            image="https://picsum.photos/170?random=6"
-          ></Block>
-          <Block
-            size="small"
-            title="S5"
-            image="https://picsum.photos/170?random=7"
-          ></Block>
-          <Block
-            size="small"
-            title="S6"
-            image="https://picsum.photos/170?random=8"
-          ></Block>
-          <Block
-            size="small"
-            title="S7"
-            image="https://picsum.photos/170?random=9"
-          ></Block>
+          {blocks.map((block, index) => (
+            <Block
+              key={index}
+              size={block.size}
+              title={block.title}
+              image={block.image}
+            />
+          ))}
         </div>
       </div>
       <Menu />
--- src/components/SmallBlock.jsx
+++ src/components/SmallBlock.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const SmallBlock = ({ title, image }) => {
+  const [background, setBackground] = useState(false);
+
+  function handleClick() {
+    console.log("CLICKED", title);
+    setBackground(!background);
+  }
+
   return (
-    <div className="Block SmallBlock">
+    <div className="Block SmallBlock" onClick={handleClick}>
       <p className="BlockTitle">{title ? title : "BlockTitle"}</p>
       <img
         className="BlockImage BlockImageSmall"
         alt="IMAGE/GIF"
         src={image}
       ></img>
+      {/* {background ? <div className="BlockBackground"></div> : ""} */}
     </div>
   );
 };
